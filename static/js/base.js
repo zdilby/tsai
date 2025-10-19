c6641ab5297b089b83c63cff808f2648139b56bf
zdilby <zdilby@gmail.com>
2025-10-20 01:05:01 +0800

    fix token loss issues when switching between development and production environments bug

--- static/js/base.js
+++ static/js/base.js
@@ -1,18 +1,7 @@
 function authFetch(url, options = {}) {
-    const token = localStorage.getItem("token");
-    // 如果没有 token，直接跳转到登录页
-    if (!token) {
-        window.location.href = "/account/login";
-        // 阻止后续请求
-        return Promise.reject(new Error("No token, redirecting to login"));
-    }
-    const headers = options.headers || {};
     return fetch(url, {
         ...options,
-        headers: {
-            ...headers,
-            Authorization: `Bearer ${token}`
-        }
+        credentials: "include",
     }).then(resp => {
 		if (resp.status === 401) {
 			window.location.href = "/account/login";
